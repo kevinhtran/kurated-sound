b3e5782e3a5ddb7e15758dcabc0dc5b9bb5e8af6
kevinhtran <kevdeveloper7@gmail.com>
2020-11-15 20:51:03 -0800

    changed navigation bar file name in order to import Navbar from react-bootstrap library. import Nav and Navdropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css';
-import Navbar from './components/Navbar';
+import NaviBar from './components/Navibar';
 import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NaviBar />
     </Router>
   );
 }
