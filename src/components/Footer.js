e41536617d0d02c9a1174931e0636f5caf03b17d
kevinhtran <kevdeveloper7@gmail.com>
2020-11-25 19:09:32 -0800

    editing style of footer to be more supplemental

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,20 +8,21 @@ const Footer = () => {
         <div className='row'>
           {/* Col 1 */}
           <div className='col'>
-            <h4>Kurated Sound</h4>
-            <ul className='list-unstyled'> 
-              <li>408-999-9999</li>
-              <li>San Jose, CA.</li>
-              <li>kuratedsound@gmail.com</li>
-            </ul>
+            <h4 className='center-block'>Kurated Sound</h4>
+              <ul className='list-unstyled center-block'>
+                  <li className='d-inline'><a href='https://www.instagram.com/kuratedsound/'><i class="fab fa-instagram-square fa-fw"></i></a></li>
+                  <li className='d-inline'><a href='https://www.facebook.com/profile.php?id=100009945706613'><i class="fab fa-facebook-square fa-fw"></i></a></li>
+                  <li className='d-inline'><a href='https://www.yelp.com/kuratedsound'><i class="fab fa-yelp fa-fw"></i></a></li>
+                  <li className='d-inline'><a href='https://www.youtube.com/kuratedsound'><i class="fab fa-youtube-square fa-fw"></i></a></li>
+              </ul>
           </div>
           {/* Col 2 */}
           <div className='col'>
-            <h4>Socials</h4>
+            <h4>STUFF</h4>
             <ul className='list-unstyled'>
-              <li><a href='https://www.instagram.com/kuratedsound/'>Instagram</a></li>
-              <li><a href='https://www.facebook.com/profile.php?id=100009945706613'>Facebook</a></li>
-              <li><a href='https://www.youtube.com/kuratedsound'>Youtube</a></li>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
             </ul>
           </div>
           {/* Col 3 */}
@@ -33,9 +34,18 @@ const Footer = () => {
               <li>Three</li>
             </ul>
           </div>
+          {/* Col 4 */}
+          <div className='col'>
+            <h4>STUFF</h4>
+            <ul className='list-unstyled'>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+            </ul>
+          </div>
         </div>
         <div className='row'>
-          <p className='col-sm' style={{alignSelf: "center"}}>
+          <p className='col-sm text-center'>
             &copy;{new Date().getFullYear()} Kurated Sound Inc | All Rights Reserved | Terms Of Services | Privacy
           </p>
         </div>
