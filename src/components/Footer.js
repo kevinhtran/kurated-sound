ff54619627712e66b7224e10a1e572e67cfa5179
kevinhtran <kevdeveloper7@gmail.com>
2020-11-25 19:32:38 -0800

    minor edits to styling and over structure of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,43 +10,43 @@ const Footer = () => {
           <div className='col'>
             <h4 className='center-block'>Kurated Sound</h4>
               <ul className='list-unstyled center-block'>
-                  <li className='d-inline'><a href='https://www.instagram.com/kuratedsound/'><i class="fab fa-instagram-square fa-fw"></i></a></li>
-                  <li className='d-inline'><a href='https://www.facebook.com/profile.php?id=100009945706613'><i class="fab fa-facebook-square fa-fw"></i></a></li>
+                  <li className='d-inline'><a href='https://www.instagram.com/kuratedsound/'><i class="fab fa-instagram fa-fw"></i></a></li>
+                  <li className='d-inline'><a href='https://www.facebook.com/profile.php?id=100009945706613'><i class="fab fa-facebook-f fa-fw"></i></a></li>
                   <li className='d-inline'><a href='https://www.yelp.com/kuratedsound'><i class="fab fa-yelp fa-fw"></i></a></li>
-                  <li className='d-inline'><a href='https://www.youtube.com/kuratedsound'><i class="fab fa-youtube-square fa-fw"></i></a></li>
+                  <li className='d-inline'><a href='https://www.youtube.com/kuratedsound'><i class="fab fa-youtube fa-fw"></i></a></li>
               </ul>
           </div>
           {/* Col 2 */}
           <div className='col'>
-            <h4>STUFF</h4>
+            <h4>Explore</h4>
             <ul className='list-unstyled'>
-              <li>One</li>
-              <li>Two</li>
-              <li>Three</li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact Us</li>
+              <li>Sign Up</li>
             </ul>
           </div>
           {/* Col 3 */}
           <div className='col'>
-            <h4>STUFF</h4>
+            <h4>Contact Us</h4>
             <ul className='list-unstyled'>
-              <li>One</li>
-              <li>Two</li>
-              <li>Three</li>
+              <li>PO BOX 99999 CA.</li>
+              <li>(408)999-9999</li>
+              <li>kuratedsound@gmail.com</li>
             </ul>
           </div>
           {/* Col 4 */}
           <div className='col'>
-            <h4>STUFF</h4>
+            <h4>Subscribe to us:</h4>
             <ul className='list-unstyled'>
-              <li>One</li>
-              <li>Two</li>
-              <li>Three</li>
+              <li>Enter email here *form goes here_______*</li>
             </ul>
           </div>
         </div>
         <div className='row'>
           <p className='col-sm text-center'>
-            &copy;{new Date().getFullYear()} Kurated Sound Inc | All Rights Reserved | Terms Of Services | Privacy
+            &copy;{new Date().getFullYear()} Kurated Sound Inc., All Rights Reserved.
           </p>
         </div>
       </div>
