ab991872509c4444bed63c0fe59db0f21269cd5f
kevinhtran <kevdeveloper7@gmail.com>
2020-12-04 16:53:18 -0800

    created parent container with child containers to have two containers as a row

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,34 +7,14 @@ import Button from 'react-bootstrap/Button';
 const Footer = () => {
   return (
     <div className='main-footer'>
+      <div className='parent-container d-flex'>
+      {/* 1st child container with logo, explore, fun stuff col */}
       <div className='container'>
         <div className='row'>
           {/* Col 1 */}
-          {/* Company Logo/Name and brand slogan */}
-          <div className='col-md-2 border-1'>
-            <h5 className='text-muted'>Kurated Sound</h5>
-          </div>
-          {/* Col 2 */}
-          {/* Explore/Redirect Column */}
-          <div className='col-md-2 border-1'>
-            <h5 className='text-muted'>Explore</h5>
-            <ul className='list-unstyled'>
-              <li><a href='../' className='text-muted'>Home</a></li>
-              <li><a href='../About' className='text-muted'>About</a></li>
-              <li><a href='../Services' className='text-muted'>Services</a></li>
-              <li><a href='../Contact' className='text-muted'>Contact Us</a></li>
-              <li><a href='../SignUp' className='text-muted'>Sign Up</a></li>
-            </ul>
-          </div>
-          {/* Col 3 */}
-          {/* Contact Column */}
-          <div className='col-md-3 border-1'>
-            <h5 className='text-muted'>Contact Us</h5>
-            <p className='text-muted'>kuratedsound@gmail.com</p>
-          </div>
-          {/* Col 4 */}
-          <div className='col-md-5 justify-content-center d-flex border-1'>
-            {/* Social Media Accounts */}
+          {/* Company Logo/Name and brand slogan & social media accounts */}
+          <div className='col-lg-5 col-md-2 border-1'>
+            <h5 className='lead'>Kurated Sound</h5>
             <div className='col'>
               <ul className='list-unstyled'>
                 <li className='d-inline'><a href='https://www.instagram.com/kuratedsound/'><i class="fab fa-instagram fa-fw"></i></a></li>
@@ -42,6 +22,37 @@ const Footer = () => {
                 <li className='d-inline'><a href='https://www.yelp.com/kuratedsound'><i class="fab fa-yelp fa-fw"></i></a></li>
                 <li className='d-inline'><a href='https://www.youtube.com/kuratedsound'><i class="fab fa-youtube fa-fw"></i></a></li>
               </ul>
+            </div>
+          </div>
+          {/* Col 2 */}
+          {/* Explore/Redirect Column */}
+          <div className='col-lg-3 col-md-2 border-1'>
+            <h5 className='lead'>Explore</h5>
+            <ul className='list-unstyled'>
+              <li><a href='../' className=''>Home</a></li>
+              <li><a href='../About' className=''>About</a></li>
+              <li><a href='../Services' className=''>Services</a></li>
+              <li><a href='../Contact' className=''>Contact Us</a></li>
+              <li><a href='../SignUp' className=''>Sign Up</a></li>
+            </ul>
+          </div>
+          {/* Col 3 */}
+          {/* Contact Column */}
+          <div className='col-lg-3 col-md-3 border-1'>
+            <h5 className='lead'>Fun Stuff</h5>
+            <p className=''>Electronic music</p>
+            <p className=''>Events</p>
+            <p className=''>Equipment</p>
+            <p className=''>DJing</p>
+          </div>
+        </div>
+      </div>
+      {/* 2nd container with subscribe email form */}
+      <div className='container'>
+          <div className='col-lg-6 col-md-5 justify-content-center d-flex border-1'>
+            <div className='col'>
+              <p className='lead'>Suscribe for news!</p>
+              {/* button goes here with 'I agree to the terms and condition, privacy, and cookie policy' */}
             </div>
             {/* Input For Email Subscription */}
             <div className='col'>
@@ -57,15 +68,18 @@ const Footer = () => {
               </InputGroup>
             </div>
           </div>
-        </div>
+      </div>
+      </div>
+      {/* Alone container with copyright and designed by text. Not apart of parent container */}
+      <div className='container'>
         <div className='row'>
-          <p className='col text-center small text-muted'>
-            &copy;{new Date().getFullYear()} Kurated Sound Inc., All Rights Reserved.
+          <p className='col text-center small lead'>
+            Designed by Kevin Tran
           </p>
         </div>
         <div className='row'>
-          <p className='col text-center small text-muted'>
-            Designed by Kevin Tran
+          <p className='col text-center small lead'>
+            &copy;{new Date().getFullYear()} Kurated Sound Inc., All Rights Reserved.
           </p>
         </div>
       </div>
