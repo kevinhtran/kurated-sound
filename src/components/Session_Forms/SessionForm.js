8841b2340c52771ce8e40a6829c8eaed9445c3e8
Vince <53957319+vincengai@users.noreply.github.com>
2020-12-14 13:44:06 -0800

    updated the import/exports of all files after filetree restructuring

--- src/components/Session_Forms/SessionForm.js
+++ src/components/Session_Forms/SessionForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Form  from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const EmailForm = () => {
+const SessionForm = () => {
   return (
     <>
       <Form>
@@ -29,4 +29,4 @@ const EmailForm = () => {
   )
 };
 
-export default EmailForm;
+export default SessionForm;
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,12 @@
 // export { default as Products } from './Products/Products';
 // export { default as Cart } from './Products/Cart';
 
-export { default as }
+export { default as Home } from './Home/Home';
+export { default as About } from './About/About';
+export { default as Services } from './Services/Services';
+export { default as Contact } from './Contact/Contact';
+export { default as SignUp } from './Session_Forms/SignUp';
+export { default as SessionForm } from './Session_Forms/SessionForm';
+export { default as NaviBar } from './Navibar/Navibar';
+export { default as Jumbo } from './Home/Jumbo';
+export { default as Footer } from './Footer/Footer';
